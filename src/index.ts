b6e1a48a492e844913771702669aa40c1352579e
CoreNion <coreinion@gmail.com>
2023-09-04 22:17:53 +0900

    Init discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+import { Message, Client, GatewayIntentBits } from 'discord.js';
+
+import { config } from 'dotenv';
+config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages] });
+
+client.on('messageCreate', (message : Message) => {
+  if(message.author.bot) return;
+
+  console.log(message.content)
+
+  if(message.content === "Hello") {
+    message.channel.send("Hello");
+  }
+});
+
+client.on('ready', async () => {
+  if(client.user === null) return;
+  const startMessage = `Logged in as ${client.user.tag}!`;
+  console.log(startMessage);
+
+  // Send a private message to the admin
+  const admin = await client.users.fetch(process.env.ADMIN || '');
+  admin.send(startMessage);
+});
+
+client.login(process.env.TOKEN);
